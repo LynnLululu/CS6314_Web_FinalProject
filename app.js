f819e76fc31dcfda3abe527ab8952c87c5cf2c54
SCGTall <scgtall@gmail.com>
2020-11-16 14:22:29 -0600

    working on npm warning

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var cartRouter = require('./routes/cart');
+var wishlistRouter = require('./routes/wishlist');
+var ordersRouter = require('./routes/orders');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -20,6 +23,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/cart', cartRouter);
+app.use('/wishlist', wishlistRouter);
+app.use('/orders', ordersRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
